fb3887837b45d5cd4f316b0c1377cd206443f154
Caius Durling <dev@caius.name>
2018-10-28 18:47:30 +0000

    Install redis from backports on >= Debian 9
    Dotdeb no longer supplies packages for debian. https://github.com/gplessis/dotdeb-redis/issues/14 has more information on this.

--- manifests/preinstall.pp
+++ manifests/preinstall.pp
@@ -12,21 +12,57 @@ class redis::preinstall {
 
       'Debian': {
         contain ::apt
-        apt::source { 'dotdeb':
-          location => 'http://packages.dotdeb.org/',
-          release  =>  $::lsbdistcodename,
-          repos    => 'all',
-          key      => {
-            id     => '6572BBEF1B5FF28B28B706837E3F070089DF5277',
-            source => 'http://www.dotdeb.org/dotdeb.gpg',
-          },
-          include  => { 'src' => true },
-          before   => [
-            Class['apt::update'],
-            Package[$::redis::package_name],
-          ],
-        }
 
+        case $::operatingsystemmajrelease {
+          # Squeeze, Wheezy, Jessie
+          '6', '7', '8': {
+            # Assume dotdeb supplies packages
+            apt::source { 'dotdeb':
+              location => 'http://packages.dotdeb.org/',
+              release  =>  $::lsbdistcodename,
+              repos    => 'all',
+              key      => {
+                id     => '6572BBEF1B5FF28B28B706837E3F070089DF5277',
+                source => 'http://www.dotdeb.org/dotdeb.gpg',
+              },
+              include  => { 'src' => true },
+              before   => [
+                Class['apt::update'],
+                Package[$::redis::package_name],
+              ],
+            }
+          }
+
+          # Stretch onwards
+          default: {
+            # Grab from Debian backports
+            if ! defined(Apt::Source["${::lsbdistcodename}-backports"]) {
+              apt::source { "${::lsbdistcodename}-backports":
+                location => "http://ftp.debian.org/debian",
+                release  => "${::lsbdistcodename}-backports",
+                repos    => "main",
+                pin      => "-1",
+                include  => { 'src' => true },
+                before   => [
+                  Class['apt::update'],
+                  Package[$::redis::package_name],
+                ],
+              }
+            }
+
+            # Allow redis/tooling to be installed from backports
+            apt::pin { "${::lsbdistcodename}-backports-redis-server":
+              packages => "redis $::redis::package_name redis-tools $::redis::sentinel_package_name",
+              priority => 500,
+              release  => "${::lsbdistcodename}-backports",
+              before   => [
+                Class['apt::update'],
+                Package[$::redis::package_name],
+              ],
+              require  => Apt::Source["${::lsbdistcodename}-backports"],
+            }
+          }
+        }
       }
 
       'Ubuntu': {
